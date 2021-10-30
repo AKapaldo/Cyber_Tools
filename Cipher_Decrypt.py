04cc87c2878d8e3f41321756996c3f2489778656
Andrew Kapaldo <53104260+AKapaldo@users.noreply.github.com>
2021-10-29 22:56:04 -0400

    Create Cipher_Decrypt.py

---
+++ Cipher_Decrypt.py
@@ -0,0 +1,69 @@
+
+# Set lists for decrytpion and encryption
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+rot13 = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+caesar = ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
+decrypt = []
+
+# Banner
+print ("""
+******************************************
+ ____  ____   ___  ____  _  _  ____  ____ 
+(    \(  __) / __)(  _ \( \/ )(  _ \(_  _)
+ ) D ( ) _) ( (__  )   / )  /  ) __/  )(  
+(____/(____) \___)(__\_)(__/  (__)   (__) 
+
+******************************************
+ """)
+
+
+# Take text input
+text = input ("Enter text string: ")
+
+# Options
+encrypt_type = input ("\n1. ROT13\n2. Caesar (ROT3) Cipher\nEnter a number: ")
+if encrypt_type == "1":
+    encrypt_dir = input ("\n1. Cleartext to ROT13\n2. ROT13 to Cleartext\nEnter a number: ")
+    if encrypt_dir == "1":
+        for letter in text:
+            if letter == " ":
+                decrypt += " "
+            elif letter == "!" or letter == "@" or letter == "#" or letter == "$" or letter == "%" or letter == "^" or letter == "&" or letter == "*" or letter == "(" or letter == ")" or letter == "~" or letter == "`" or letter == "-" or letter == "_" or letter == "+" or letter == "=" or letter == "[" or letter == "]" or letter == "{" or letter == "}" or letter == "|" or letter == "\\" or letter == ":" or letter == ";" or letter == "'" or letter == "\"" or letter == "," or letter == "." or letter == "<" or letter == ">" or letter == "?" or letter == "/":
+                decrypt += letter
+            elif letter == "1" or letter == "2" or letter == "3" or letter == "4" or letter == "5" or letter == "6" or letter == "7" or letter == "8" or letter == "9" or letter == "0":
+                decrypt += letter
+            else:
+                index = alpha.index(letter)
+                decrypt += rot13[index]
+    elif encrypt_dir == "2":
+        for letter in text:
+            if letter == " ":
+                decrypt += " "
+            elif letter == "!" or letter == "@" or letter == "#" or letter == "$" or letter == "%" or letter == "^" or letter == "&" or letter == "*" or letter == "(" or letter == ")" or letter == "~" or letter == "`" or letter == "-" or letter == "_" or letter == "+" or letter == "=" or letter == "[" or letter == "]" or letter == "{" or letter == "}" or letter == "|" or letter == "\\" or letter == ":" or letter == ";" or letter == "'" or letter == "\"" or letter == "," or letter == "." or letter == "<" or letter == ">" or letter == "?" or letter == "/":
+                decrypt += letter
+            elif letter == "1" or letter == "2" or letter == "3" or letter == "4" or letter == "5" or letter == "6" or letter == "7" or letter == "8" or letter == "9" or letter == "0":
+                decrypt += letter
+            else:
+                index = rot13.index(letter)
+                decrypt += alpha[index]
+    else:
+        print ("Invalid entry.")
+elif encrypt_type == "2":
+    encrypt_dir = input ("\n1. Cleartext to Caesar\n2. Caesar to Cleartext\nEnter a number: ")
+    if encrypt_dir == "1":
+        for letter in text:
+            if letter == " ":
+                decrypt += " "
+            else:
+                index = alpha.index(letter)
+                decrypt += caesar[index]
+    elif encrypt_dir == "2":
+        for letter in text:
+            if letter == " ":
+                decrypt += " "
+            else:
+                index = caesar.index(letter)
+                decrypt += alpha[index]
+    else:
+        print ("Invalid entry.")
+print ("\nOutput: " + ''.join(decrypt))
