b11d8d4e9e83a3e5809b4fa5272f9883da13a1b6
Andrew Kapaldo <53104260+AKapaldo@users.noreply.github.com>
2021-10-31 13:35:25 -0400

    Update Cipher_Decrypt.py

--- Cipher_Decrypt.py
+++ Cipher_Decrypt.py
@@ -1,3 +1,42 @@
+#!/bin/python3
+
+# Import Modules
+import platform, argparse
+
+# Set Variables
+verbose = False
+if platform.system() == "Windows":
+    class colors:
+        HEADER = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+else:
+    class colors:
+        HEADER = '\033[95m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+
+header = print(colors.HEADER + "#" * 42 +
+"""
+ ____  ____   ___  ____  _  _  ____  ____ 
+(    \(  __) / __)(  _ \( \/ )(  _ \(_  _)
+ ) D ( ) _) ( (__  )   / )  /  ) __/  )(  
+(____/(____) \___)(__\_)(__/  (__)   (__) 
+
+""" + "#" * 42 + colors.ENDC)
+
+# create parser
+parser = argparse.ArgumentParser(description=header, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+# add arguments to the parser
+parser.add_argument('-t', type=str, required=False)
+
+# parse the arguments
+args = parser.parse_args()
 
 # Set lists for decrytpion and encryption
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -6,19 +45,13 @@ caesar = ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
 decrypt = []
 
 # Banner
-print ("""
-******************************************
- ____  ____   ___  ____  _  _  ____  ____ 
-(    \(  __) / __)(  _ \( \/ )(  _ \(_  _)
- ) D ( ) _) ( (__  )   / )  /  ) __/  )(  
-(____/(____) \___)(__\_)(__/  (__)   (__) 
-
-******************************************
- """)
-
+header
 
 # Take text input
-text = input ("Enter text string: ")
+if args.t == None:
+    text = input ("Enter text string: ")
+else:
+    text = open(args.t).read()
 
 # Options
 encrypt_type = input ("\n1. ROT13\n2. Caesar (ROT3) Cipher\nEnter a number: ")
@@ -47,7 +80,7 @@ if encrypt_type == "1":
                 index = rot13.index(letter)
                 decrypt += alpha[index]
     else:
-        print ("Invalid entry.")
+        print(f"{colors.FAIL}Invalid entry.{colors.ENDC}")
 elif encrypt_type == "2":
     encrypt_dir = input ("\n1. Cleartext to Caesar\n2. Caesar to Cleartext\nEnter a number: ")
     if encrypt_dir == "1":
@@ -65,5 +98,5 @@ elif encrypt_type == "2":
                 index = caesar.index(letter)
                 decrypt += alpha[index]
     else:
-        print ("Invalid entry.")
-print ("\nOutput: " + ''.join(decrypt))
+        print(f"{colors.FAIL}Invalid entry.{colors.ENDC}")
+print ("\nOutput:\n" + ''.join(decrypt))
